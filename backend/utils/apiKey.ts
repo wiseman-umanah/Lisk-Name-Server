881528e1d8b58e16519dc2d86ce35fe44b2d194e
wiseman-umanah <wisemanumanah@gmail.com>
2025-06-20 08:55:17 +0100

    Updated api key characters to 8 x 2 bytes

--- backend/utils/apiKey.ts
+++ backend/utils/apiKey.ts
@@ -3,7 +3,7 @@ import crypto from "crypto";
 const apiKeys = new Map<string, string>(); // key => address
 
 export function generateApiKey(): string {
-  return crypto.randomBytes(32).toString("hex");
+  return crypto.randomBytes(8).toString("hex");
 }
 
 export function storeApiKey(address: string, key: string) {
